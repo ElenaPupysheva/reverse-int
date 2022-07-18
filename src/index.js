319efa9289c1ebbdc868f54ae1587b6c96a39aa0
Elena Pupysheva <nyankem@gmail.com>
2022-07-18 16:36:20 +0300

    fix: reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) {
+        n=n*(-1);
+        let l=n.toString();
+        let splits=l.split('');
+        let rev=splits.reverse();
+        let newNum=Number(rev.join(''))
+        console.log(newNum);
+        return newNum;
+    }
+    else {let l=n.toString();
+        let splits=l.split('');
+        let rev=splits.reverse();
+        let newNum=Number(rev.join(''));
+        console.log(newNum);
+        return newNum}
 }
